e91f3e15443ec83eced07caf8ad8a2a59b396661
Rodrigo Richter <guigorichter@gmail.com>
2018-11-10 11:33:05 -0200

    ofdm

--- testes.m
+++ testes.m
@@ -17,3 +17,19 @@ bitOUT = qammod(bitsIN, M);     % OUT -> vetor complexo, cada elemento é um pont
 %turboOUT = lteTurboEncode(turboIN); % matlab fdp só implementou o turbo
 %encoding em 2014 agora to triste
 
+%------------ OFDM SENDER: ------------%
+ifft_sig=ifft(bitOUT',64);
+
+snr = 2;
+ofdm_sig=awgn(ifft_sig,snr,'measured'); % Adding white Gaussian Noise
+
+
+%------------ OFDM RECEIVER: ------------%
+recv_sig = fft(ofdm_sig,64);
+
+
+%------------ DEMODULAÇÃO: ------------%
+dem_data= qamdemod(recv_sig,M);
+
+
+
